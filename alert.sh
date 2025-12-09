a00e96821865b55d15e1c38028fb9948dfbb203d
CXJ-jzx <1158285666@qq.com>
2025-12-09 10:48:11 +0800

    Rename alert.sh to elastic/alert.sh

--- alert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# /data/flink/elastic/alert.sh（仅node01）
-ALERT_LOG=/data/flink/elastic/log/alert.log
-ALERT_CONTENT=$1
-# 写入告警日志（可扩展邮件/短信）
-echo "$(date +'%Y-%m-%d %H:%M:%S') - ALERT: ${ALERT_CONTENT}" >> ${ALERT_LOG}
-# 示例：邮件告警（需配置sendmail）
-# echo "${ALERT_CONTENT}" | mail -s "Flink弹性调度告警" your_email@xxx.com
-echo "${ALERT_CONTENT}" | mail -s "Flink弹性调度告警" 1158285666@qq.com
